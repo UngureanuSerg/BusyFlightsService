19364c3523c46203735d5c3beb132cb6b3a0cb3e
UngureanuSerg <ungureanuserg@gmail.com>
2020-01-14 22:53:51 +0200

    curation of domain using lombok

--- src/main/java/com/travix/medusa/busyflights/domain/busyflights/BusyFlightsRequest.java
+++ src/main/java/com/travix/medusa/busyflights/domain/busyflights/BusyFlightsRequest.java
@@ -1,50 +1,22 @@
 package com.travix.medusa.busyflights.domain.busyflights;
 
+import javax.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
+
+@Getter
+@Builder
 public class BusyFlightsRequest {
 
-    private String origin;
-    private String destination;
-    private String departureDate;
-    private String returnDate;
-    private int numberOfPassengers;
+  @Size(min = 3, max = 3)
+  private String origin;
+  @Size(min = 3, max = 3)
+  private String destination;
 
-    public String getOrigin() {
-        return origin;
-    }
+  private String departureDate;
+  private String returnDate;
 
-    public void setOrigin(final String origin) {
-        this.origin = origin;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(final String destination) {
-        this.destination = destination;
-    }
-
-    public String getDepartureDate() {
-        return departureDate;
-    }
-
-    public void setDepartureDate(final String departureDate) {
-        this.departureDate = departureDate;
-    }
-
-    public String getReturnDate() {
-        return returnDate;
-    }
-
-    public void setReturnDate(final String returnDate) {
-        this.returnDate = returnDate;
-    }
-
-    public int getNumberOfPassengers() {
-        return numberOfPassengers;
-    }
-
-    public void setNumberOfPassengers(final int numberOfPassengers) {
-        this.numberOfPassengers = numberOfPassengers;
-    }
+  @Range(min = 1, max = 4)
+  private int numberOfPassengers;
 }
